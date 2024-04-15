ca12c3acb875408b6cebe3849644c53cf93d1dbf
Alex Forshtat <forshtat1@gmail.com>
2024-04-15 23:37:34 +0200

    WIP: separate stack trace parsing and verkle gas cost conversion

---
+++ estimation.py
@@ -0,0 +1,138 @@
+import math
+
+HEADER_STORAGE_OFFSET = 64
+CODE_OFFSET = 128
+VERKLE_NODE_WIDTH = 256
+MAIN_STORAGE_OFFSET = 256 ** 31
+
+WITNESS_BRANCH_COST = 1900
+WITNESS_CHUNK_COST = 200
+
+COLD_SLOAD_COST = 2100
+COLD_ACCOUNT_ACCESS_COST = 2600
+
+SUBTREE_EDIT_COST = 3000
+CHUNK_EDIT_COST = 500
+CHUNK_FILL_COST = 6200
+
+
+def calculate_deployment_gas_effect(contract_size):
+    code_chunks = contract_size // 31
+    code_subtrees = math.ceil((code_chunks + CODE_OFFSET) / 256)
+    return CHUNK_FILL_COST * code_chunks + SUBTREE_EDIT_COST * code_subtrees
+
+
+def calculate_chunks_read_verkle_effect(contract_chunks, branches_access_events):
+    code_cost = 0
+    for [contract_chunk, _] in contract_chunks.items():
+        code_cost += WITNESS_CHUNK_COST
+        branch_id = contract_chunk // 256
+        if branch_id not in branches_access_events:
+            branches_access_events[branch_id] = True
+            code_cost += WITNESS_BRANCH_COST
+    return code_cost
+
+
+def calculate_slots_read_verkle_overhead(contract_slots, branches_access_events):
+    storage_cost = 0
+    for [storage_key_hex, _] in contract_slots.items():
+        storage_cost += WITNESS_CHUNK_COST
+        storage_key = int(storage_key_hex, 16)
+        # special storage for slots 0..64
+        if storage_key < (CODE_OFFSET - HEADER_STORAGE_OFFSET):
+            pos = HEADER_STORAGE_OFFSET + storage_key
+        else:
+            pos = MAIN_STORAGE_OFFSET + storage_key
+        branch_id = pos // VERKLE_NODE_WIDTH
+        if branch_id not in branches_access_events:
+            branches_access_events[branch_id] = True
+            storage_cost += WITNESS_BRANCH_COST
+
+    return storage_cost
+
+
+def calculate_slots_read_verkle_savings(contract_slots, branches_access_events):
+    return 0
+
+
+def calculate_call_opcode_verkle_savings(calls_performed):
+    return 0
+
+
+def calculate_create2_opcode_verkle_savings(contract_slots, branches_access_events):
+    return 0
+
+
+def get_name(address, names):
+    if address not in names or names[address] is None:
+        return address
+    else:
+        short_address = "(" + address[:8] + "..." + address[38:] + ")"
+        return names[address] + " " + short_address
+
+
+def print_results(name, results, total_gas_effect, dumpall):
+    print("")
+    print("Total Verkle gas effect = " + str(total_gas_effect))
+
+    for address in results:
+        result = results[address]
+        if result['code_size'] > 0:
+            codeInfo = f"bytes:{result['code_size']} chunks:{result['code_size_chunks']} max:{result['max_chunk']}"
+        else:
+            codeInfo = f"max_chunk:{result['max_chunk']}"
+
+        dumpallSuffix = ""
+        if dumpall:
+            dumpallSuffix = f", all={','.join(map(str, result['chunks'][result['addrress']].keys()))}"
+
+        print(
+            f"{name} code:({codeInfo} accessed-chunks:{result['num_chunks']}) storage-slots:{len(result['addr_slots'])} [verkle: code={result['addr_code_cost']} + storage={result['addr_storage_cost']}]{dumpallSuffix}"
+        )
+
+
+def estimate_verkle_effect(trace_data, dumpall, names):
+    total_gas_effect = 0
+    branches_access_events = {}
+    results = {}
+    # for each address in trace data
+    # branches_access_events += {0: True}
+
+    # Dump number of unique slots used by each address
+    chunks = trace_data['chunks']
+    for addr in chunks:
+        max_chunk = max(chunks[addr].keys())
+        num_chunks = len(chunks[addr])
+
+        addr_slots = {}
+        if addr in trace_data['slots']:
+            addr_slots = trace_data['slots'][addr]
+        addr_code_cost = calculate_chunks_read_verkle_effect(chunks[addr], branches_access_events)
+
+        addr_storage_cost = 0
+        if addr in trace_data['slots']:
+            addr_storage_cost = calculate_slots_read_verkle_overhead(trace_data['slots'][addr], branches_access_events)
+
+        #  TODO
+        addr_storage_savings = 0
+
+        #  TODO
+        call_savings = 0
+
+        code_size_chunks = (trace_data['code_sizes'][addr] + 30) // 31
+
+        result = {
+            'max_chunk': max_chunk,
+            'num_chunks': num_chunks,
+            'addr_slots': addr_slots,
+            'addr_code_cost': addr_code_cost,
+            'addr_storage_cost': addr_storage_cost,
+            'addr_storage_savings': addr_storage_savings,
+            'call_savings': call_savings,
+            'code_size': trace_data['code_sizes'][addr],
+            'code_size_chunks': code_size_chunks
+        }
+
+        results[addr] = result
+        name = get_name(addr, names)
+        print_results(name, results, total_gas_effect, dumpall)
---
+++ verkle_gas_estimator.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+import json
+import os
+import re
+import subprocess
+import sys
+
+from estimation import estimate_verkle_effect, print_results
+
+
+# Function to run cast command and return output
+def run_cast(command):
+    cmd = f"{cast_executable} {command}"
+    return subprocess.check_output(cmd, shell=True, text=True)
+
+
+names = {}
+test_cases = []
+
+dumpall = False
+debug = os.environ.get("DEBUG") is not None
+extraDebug = False
+cast_executable = "cast"
+
+
+def usage():
+    print(f"usage: {sys.argv[0]} [options] {{tx|file}} [-r network]")
+    print("count storage 'chunks' (blocks of 31 bytes) of each contract in a transaction.")
+    print("Options:")
+    print("  -a dump all chunks, not only count/max")
+    print("  -c {cast-path} use specified 'cast' implementation ")
+    print("  -contracts match contract addresses with names in given JSON file")
+    print("  -multiple iterate over transactions in a JSON results file and calculate results for each entry")
+    print("Parameters:")
+    print("  tx - tx to read. It (and all following params) are passed directly into `cast run -t --quick`")
+    print("  file - if the first param is an existing file, it is read instead.")
+    print("         The file should be the output of `cast run -t --quick {tx} > FILE`")
+    sys.exit(1)
+
+
+args = sys.argv[1:]
+if args == []:
+    args = ["-h"]
+
+while len(args) > 0 and re.match("^-", args[0]):
+    opt = args.pop(0)
+    if opt == "-h":
+        usage()
+    elif opt == "-c":
+        cast_executable = args.pop(0)
+    elif opt == "-a":
+        dumpall = True
+    elif opt == "-d":
+        debug = True
+    elif opt == "-dd":
+        extraDebug = True
+    elif opt == "-contracts":
+        f = open(args.pop(0))
+        file = json.load(f)
+        names = file["contracts"]
+    elif opt == "-multiple":
+        f = open(args.pop(0))
+        test_cases = json.load(f)
+    else:
+        raise Exception("Unknown option " + opt)
+
+
+def parse_trace_results(case, output):
+    print(f"Evaluating transaction {case['txHash']}")
+    trace_data = {}
+
+    addrs = []
+    lastdepth = 0
+    chunks = {}
+    slots = {}
+    code_sizes = {}
+
+    # line = None
+    # lastGas = None
+    # lastRefund = None
+
+    lines = output.splitlines()
+    for lineNumber in range(len(lines)):
+        line = lines[lineNumber]
+        if "Traces:" in line:
+            break
+        if "CREATE CALL:" in line:
+            continue
+        if "SM CALL" in line:
+            (sm_context_address, sm_code_address) = re.search("address: (\w+).*code_address: (\w+)", line).groups()
+            continue
+
+        if "depth:" in line:
+
+            # depth:1, PC:0, gas:0x10c631(1099313), OPCODE: "PUSH1"(96)  refund:0x0(0) Stack:[], Data size:0, Data: 0x
+            (depth, pc, lineGas, opcode, lineRefund, stackStr) = re.search(
+                "depth:(\d+).*PC:(\d+).*gas:\w+\((\w+)\).*OPCODE: \"(\w+)\".*refund:\w+\((\w+)\).*Stack:\[(.*)\]",
+                line).groups()
+
+            gas = None
+            refund = None
+            # cannot check *CALL: next opcode is in different context
+            if "depth:" in lines[lineNumber + 1]:
+                (nextGas, nextRefund, nextStackStr) = re.search(
+                    "gas:\w+\((\w+)\).*refund:\w+\((\w+)\).*Stack:\[(.*)\]",
+                    lines[lineNumber + 1]).groups()
+
+                # gas, refund by this opcode
+                gas = int(lineGas) - int(nextGas)
+                refund = int(nextRefund) - int(lineRefund)
+
+            stack = stackStr.replace("_U256", "").split(", ")[-2:]
+            if opcode == "SSTORE":
+                (val, storageSlot) = stack
+                if context_address not in slots:
+                    slots[context_address] = {}
+                slots[context_address][storageSlot] = True
+                if debug: print(
+                    f"{opcode} context={context_address} slot={storageSlot} gas={gas} refund={refund} val={val}")
+            if opcode == "SLOAD":
+                (storageSlot,) = stack[-1:]
+                if context_address not in slots:
+                    slots[context_address] = {}
+                slots[context_address][storageSlot] = True
+                nextStack = nextStackStr.replace("_U256", "").split(", ")[-2:]
+                if debug: print(
+                    f"{opcode} context={context_address} slot={storageSlot} gas={gas} refund={refund}, ret={nextStack[-1:][0]}")
+            if depth:
+                depth = int(depth)
+                if depth == lastdepth + 1:
+                    addrs.append((sm_code_address, sm_context_address))
+                    (addr, context_address) = addrs[-1]
+                if depth == lastdepth - 1:
+                    addrs.pop()
+                    (addr, context_address) = addrs[-1]
+                pc = int(pc)
+                chunk = pc // 31
+                if addr not in chunks:
+                    chunks[addr] = {}
+                chunks[addr][chunk] = chunks[addr].get(chunk, 0) + 1
+                if extraDebug:
+                    print(f"{addr}, {chunk}, {pc}, {opcode}, {gas}, {stack}")
+                lastdepth = depth
+
+    for address in chunks:
+        code_sizes[address] = 0
+        try:
+            code_sizes[address] = int(run_cast(f"codesize {address} 2>/dev/null").strip())
+        except:
+            pass
+
+    trace_data['code_sizes'] = code_sizes
+    trace_data['chunks'] = chunks
+    trace_data['slots'] = slots
+
+    return trace_data
+
+
+def evaluate_test_case(case):
+    output = run_cast(f"run -t --quick {case['txHash']}")
+    trace_results = parse_trace_results(case, output)
+    verkle_results = estimate_verkle_effect(trace_results, dumpall, names)
+    print_results(case['name'], verkle_results, dumpall)
+
+
+# Check if file exists, read file instead of running cast run
+if len(args) > 0 and os.path.exists(args[0]):
+    with open(args[0], 'r') as f:
+        cast_output = f.read()
+        estimate_verkle_effect({}, cast_output, names)
+elif len(test_cases) > 0:
+    for test_case in test_cases:
+        evaluate_test_case(test_case)
+else:
+    argStr = " ".join(args)
+    cast_output = run_cast(f"run -t --quick {argStr}")
+    estimate_verkle_effect({}, cast_output, names)
