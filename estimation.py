0ac72d279f45a1fabd1c80df04b3c6abc9a05a5c
Alex Forshtat <forshtat1@gmail.com>
2024-04-16 17:24:55 +0200

    Implement EIP-4762 read and write events cost calculation

--- estimation.py
+++ estimation.py
@@ -15,6 +15,10 @@ SUBTREE_EDIT_COST = 3000
 CHUNK_EDIT_COST = 500
 CHUNK_FILL_COST = 6200
 
+#  Current (EIP-2929, EIP-2200) constants
+SSTORE_SET_GAS = 20000
+WARM_STORAGE_READ_COST = 100
+
 
 def calculate_deployment_gas_effect(contract_size):
     code_chunks = contract_size // 31
@@ -22,7 +26,9 @@ def calculate_deployment_gas_effect(contract_size):
     return CHUNK_FILL_COST * code_chunks + SUBTREE_EDIT_COST * code_subtrees
 
 
-def calculate_chunks_read_verkle_effect(contract_chunks, branches_access_events):
+def calculate_chunks_read_verkle_effect(contract_chunks):
+    branches_access_events = {0: True}  # subtree 0 is always initialized
+
     code_cost = 0
     for [contract_chunk, _] in contract_chunks.items():
         code_cost += WITNESS_CHUNK_COST
@@ -33,33 +39,73 @@ def calculate_chunks_read_verkle_effect(contract_chunks, branches_access_events)
     return code_cost
 
 
-def calculate_slots_read_verkle_overhead(contract_slots, branches_access_events):
-    storage_cost = 0
-    for [storage_key_hex, _] in contract_slots.items():
-        storage_cost += WITNESS_CHUNK_COST
-        storage_key = int(storage_key_hex, 16)
-        # special storage for slots 0..64
-        if storage_key < (CODE_OFFSET - HEADER_STORAGE_OFFSET):
-            pos = HEADER_STORAGE_OFFSET + storage_key
-        else:
-            pos = MAIN_STORAGE_OFFSET + storage_key
-        branch_id = pos // VERKLE_NODE_WIDTH
-        if branch_id not in branches_access_events:
-            branches_access_events[branch_id] = True
-            storage_cost += WITNESS_BRANCH_COST
+def get_slot_verkle_id(storage_key_hex):
+    storage_key = int(storage_key_hex, 16)
+    # special storage for slots 0..64
+    if storage_key < (CODE_OFFSET - HEADER_STORAGE_OFFSET):
+        pos = HEADER_STORAGE_OFFSET + storage_key
+    else:
+        pos = MAIN_STORAGE_OFFSET + storage_key
+    branch_id = pos // VERKLE_NODE_WIDTH
+    sub_id = pos % VERKLE_NODE_WIDTH
 
-    return storage_cost
+    return [branch_id, sub_id]
 
 
-def calculate_slots_read_verkle_savings(contract_slots, branches_access_events):
-    return 0
+#  subtract current costs and apply new costs
+def calculate_slots_verkle_difference(contract_slots):
+    accessed_subtrees = {0: True}  # subtree 0 is always initialized
+    accessed_leaves = {}
+    edited_subtrees = {}
+    edited_leaves = {}
+
+    new_costs = 0
+    old_cost = 0
+    for slot_id in contract_slots:
+        slot = contract_slots[slot_id]
+        [branch_id, sub_id] = get_slot_verkle_id(slot_id)
+        for opcode in slot:
+            old_cost += opcode['gas']
+            if opcode['opcode'] == 'SSTORE':
+                if branch_id in edited_subtrees and sub_id in edited_leaves:
+                    new_costs += WARM_STORAGE_READ_COST  # this is not explicitly specified by EIP-4762
+                if branch_id not in edited_subtrees:
+                    new_costs += SUBTREE_EDIT_COST
+                    edited_subtrees[branch_id] = True
+                if sub_id not in edited_leaves:
+                    new_costs += CHUNK_EDIT_COST
+                    edited_leaves[branch_id] = True
+                    if opcode['gas'] == SSTORE_SET_GAS:  # considering pre-verkle value 0 as equivalent to 'None'
+                        new_costs += CHUNK_FILL_COST
+
+            elif opcode['opcode'] == 'SLOAD':
+                if branch_id in accessed_subtrees and sub_id in accessed_leaves:
+                    new_costs += WARM_STORAGE_READ_COST  # this is not explicitly specified by EIP-4762
+                if branch_id not in accessed_subtrees:
+                    new_costs += WITNESS_BRANCH_COST
+                    accessed_subtrees[branch_id] = True
+                if sub_id not in accessed_leaves:
+                    new_costs += WITNESS_CHUNK_COST
+                    accessed_leaves[branch_id] = True
+            else:
+                raise Exception("Unknown opcode " + slot['opcode'])
+    return new_costs - old_cost
+
+
+#  it appears that all the refund-based logic in EIP-2200 and EIP-2929 is removed in EIP-4762
+def calculate_slots_read_verkle_removed_refunds(contract_slots):
+    refunds = 0
+    for slot_id in contract_slots:
+        for opcode in contract_slots[slot_id]:
+            refunds += opcode['refund']
+    return refunds
 
 
 def calculate_call_opcode_verkle_savings(calls_performed):
     return 0
 
 
-def calculate_create2_opcode_verkle_savings(contract_slots, branches_access_events):
+def calculate_create2_opcode_verkle_savings(contract_slots):
     return 0
 
 
@@ -72,14 +118,10 @@ def get_name(address, names):
 
 
 def estimate_verkle_effect(trace_data, names):
-    total_gas_effect = 0
-    branches_access_events = {}
     results = {
         'per_contract_result': {},
         'total_gas_effect': 0
     }
-    # for each address in trace data
-    # branches_access_events += {0: True}
 
     # Dump number of unique slots used by each address
     chunks = trace_data['chunks']
@@ -87,24 +129,16 @@ def estimate_verkle_effect(trace_data, names):
         max_chunk = max(chunks[addr].keys())
         num_chunks = len(chunks[addr])
 
-        addr_slots = {}
+        addr_code_cost = calculate_chunks_read_verkle_effect(chunks[addr])
+
+        addr_storage_difference = 0
+        addr_storage_removed_refund = 0
         if addr in trace_data['slots']:
             addr_slots = trace_data['slots'][addr]
+            addr_storage_difference = calculate_slots_verkle_difference(addr_slots)
+            addr_storage_removed_refund = calculate_slots_read_verkle_removed_refunds(addr_slots)
 
-        if addr not in branches_access_events:
-            branches_access_events[addr] = {}
-
-        addr_code_cost = calculate_chunks_read_verkle_effect(chunks[addr], branches_access_events[addr])
-
-        addr_storage_cost = 0
-        if addr in trace_data['slots']:
-            addr_storage_cost = calculate_slots_read_verkle_overhead(trace_data['slots'][addr], branches_access_events[addr])
-
-        #  TODO
-        addr_storage_savings = 0
-
-        #  TODO
-        call_savings = 0
+        call_savings = calculate_call_opcode_verkle_savings(trace_data)
 
         code_size_chunks = (trace_data['code_sizes'][addr] + 30) // 31
 
@@ -114,20 +148,22 @@ def estimate_verkle_effect(trace_data, names):
             'contract_name': contract_name,
             'max_chunk': max_chunk,
             'num_chunks': num_chunks,
-            'addr_slots': addr_slots,
-            'addr_storage_reads': -1,
-            'addr_storage_edits': -1,
-            'addr_storage_fills': -1,
             'call_opcode_with_value_count': -1,
             'address_touching_opcode_count': -1,
             'create2_opcode_bytes_written': -1,
             'addr_code_cost': addr_code_cost,
-            'addr_storage_cost': addr_storage_cost,
-            'addr_storage_savings': addr_storage_savings,
+            'addr_storage_difference': addr_storage_difference,
+            # EIP-2200 heavily relies on refunds which is superseded by EIP-4762
+            'addr_storage_removed_refund': addr_storage_removed_refund,
             'call_savings': call_savings,
             'code_size': trace_data['code_sizes'][addr],
             'code_size_chunks': code_size_chunks
         }
         results['per_contract_result'][addr] = per_contract_result
-        results['total_gas_effect'] = total_gas_effect
+        results['total_gas_effect'] += \
+            (addr_code_cost +
+             addr_storage_difference -
+             addr_storage_removed_refund -
+             call_savings
+             )
     return results
--- print_results.py
+++ print_results.py
@@ -1,31 +1,33 @@
 def print_results(results, dumpall):
     for address in results['per_contract_result']:
         result = results['per_contract_result'][address]
-        if result['code_size'] == -1:
-            code_info = f"max_chunk:{result['max_chunk']}"
-        elif result['code_size'] > 0:
-            code_info = f"chunks:{result['code_size_chunks']} bytes:{result['code_size']} max:{result['max_chunk']}"
-        else:
-            code_info = "failed to get contract size"
+        # if result['code_size'] == -1:
+        #     code_info = f"max_chunk:{result['max_chunk']}"
+        # elif result['code_size'] > 0:
+        #     code_info = f"chunks:{result['code_size_chunks']} bytes:{result['code_size']} max:{result['max_chunk']}"
+        # else:
+        #     code_info = "failed to get contract size"
 
         print(result['contract_name'])
-        print('Code Access Details:')
+        print('Code Access')
         print(f"accessed chunks: {result['num_chunks']}")
-        print(code_info)
         print(f"accessed chunks verkle cost: {result['addr_code_cost']}")
-        print('Storage Access Details:')
-        print(f"storage slots reads: {result['addr_storage_reads']}")
-        print(f"storage slots edits: {result['addr_storage_edits']}")
-        print(f"storage slots fills: {result['addr_storage_fills']}")
-        print(f"storage slots verkle cost: {result['addr_storage_cost']}")
-        print(f"storage slots verkle savings: {result['addr_storage_savings']}")
-        print('Address Touching Opcodes:')
+        print('')
+        print('Storage Access')
+        print(f"storage slots verkle cost (diff): {result['addr_storage_difference']}")
+        print('')
+        print('Address Warming')
         print(f"CALL with value: {result['call_opcode_with_value_count']}")
         print(f"Others: {result['address_touching_opcode_count']}")
-        print('CREATE2 Opcode:')
+        print('')
+        print('CREATE2')
         print(f"bytes written:{result['create2_opcode_bytes_written']}")
         if dumpall:
             print(f", all chunks={','.join(map(str, result['chunks'][result['addrress']].keys()))}")
         print("")
         print("")
     print("Total Verkle gas effect = " + str(results['total_gas_effect']))
+
+
+def print_storage_access():
+    return 0
